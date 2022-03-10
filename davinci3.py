147839430f19c7022cc170fd07700cd6764c3f49
bbs-mihoyo <98314553+bbs-mihoyo@users.noreply.github.com>
2022-03-09 19:19:20 -0800

    Add files via upload

--- davinci3.py
+++ davinci3.py
@@ -1,49 +1,4 @@
 from davinci import Card, Player, Game2
-import random
-
-dp = {}
-
-def to_str(d1: list[Card], d2: list[Card]):
-    res = ""
-    for card in d1:
-        num = card.val*2
-        if card.visible: num += 1
-        res += str(num)
-    res += "&"
-    for card in d2:
-        num = card.val*2
-        if card.visible: num += 1
-        res += str(num)
-    return res
-
-def possible_combinations(visibles: set, d2: list[Card]):
-    if len([card for card in d2 if not card.visible]) == 0:
-        res = set([i for i in range(24)])
-        return [], res.difference(visibles)
-    res = []
-    for i in range(len(d2)):
-        card = d2[i]
-        res.append(card.val)
-        if card.visible: continue
-        #if not card.possible_value: continue
-        card.visible = True
-        
-        for val in card.possible_value:
-            if i > 0 and d2[i-1].val >= val: continue
-            if card.val in visibles: continue
-            card.val = val
-            visibles.add(val)
-            for res2, remain in possible_combinations(visibles, d2):
-                yield res + res2, remain
-            visibles.discard(val)
-        res = []
-        card.visible = False
-
-def possible_deck(visibles: set, d2: list[Card], p4: Player):
-    for res, remain in possible_combinations(visibles, d2):
-        deck = p4.deck
-
-        yield [Card(i) for i in res], [Card(i) for i in remain]
 
 def copy_card(c: Card):
     res = Card(c.val)
@@ -62,6 +17,7 @@ def copy_player(p1: Player, p2: Player):
     p4.other = p3
     return p3, p4
 
+# calculate the probability of winning for player1 in a state when player2 guess right
 def eval2_reguess(p1: Player, p2: Player, remain: list[Card], val:int):
     if not p1.alive(): return 1
     if not p2.alive(): return 0
@@ -86,14 +42,18 @@ def eval2_reguess(p1: Player, p2: Player, remain: list[Card], val:int):
     else:
         return eval1(p3, p4, remain)
 
+# calculate the probability of winning for player1 in a state that is player2's turn
 def eval2(p1: Player, p2: Player, remain: list[Card]):
     if len(remain) == 0: return 0
     if not p1.alive(): return 1
     if not p2.alive(): return 0
+
     p3, p4 = copy_player(p1, p2)
     remain = [copy_card(card) for card in remain]
+
     new_card = remain.pop()
     p4.pickup(new_card)
+
     guess_card = p4.guessing_list()[0]
     n = len(guess_card.possible_value)
     guess_card.visible = True
@@ -103,6 +63,7 @@ def eval2(p1: Player, p2: Player, remain: list[Card]):
     wrong_win_rate = eval1(p3, p4, remain)
     return (right_win_rate + wrong_win_rate*(n-1))/n
 
+# calculate the probability of winning for player1 in a state when player1 guess right
 def eval1_reguess(p1: Player, p2: Player, remain: list[Card], val, return_choice = False):
     if not p1.alive(): return 1
     if not p2.alive(): return 0
@@ -129,7 +90,7 @@ def eval1_reguess(p1: Player, p2: Player, remain: list[Card], val, return_choice
     if return_choice: return reguess_win_rate < skip_win_rate
     else: return min(reguess_win_rate, skip_win_rate)
         
-
+# calculate the probability of winning for player1 in a state that is player1's turn
 def eval1(p1: Player, p2: Player, remain: list[Card]):
     if len(remain) == 0: return 1
     if not p1.alive(): return 1
@@ -161,11 +122,14 @@ class Game114514(Game2):
                     break
         else: card.show()
     
+    # the logic that used to identify if player1 should make extra guess
     def continues(self, player1, card):
         lst = player1.guessing_list()
         if len(lst) < 1: return False
         values = len(lst[0].possible_value)
         choices = len([card for card in player1.other.deck if not card.visible])
+        #if values == 1: return True
+        #return eval1_reguess(self.player1, self.player2, self.deck, card.val, return_choice=True)
         if values > 2: return False
         if values == 2 and choices > player1.alpha: return False
         if values == 2: return eval1_reguess(self.player1, self.player2, self.deck, card.val, return_choice=True)
@@ -183,6 +147,7 @@ class Game114514(Game2):
                     break
         else: card.show()
 
+    # the logic that used to identify if player2 should make extra guess
     def continues2(self, player1):
         lst = player1.guessing_list()
         if len(lst) < 1: return False
@@ -192,16 +157,16 @@ class Game114514(Game2):
         if values == 2 and choices > player1.alpha: return False
         return True
 
+#run 10000 simulation, while each player has 5000 times to start first.
 def simluation(alpha1, alpha2):
     result = ""
     for i in range(5000):
-        #print("new game!!!!\n\n\n\n\n")
         result += str(Game114514(alpha1, alpha2).play())
-        #print("new game!!!!\n\n\n\n\n")
         result += str(Game114514(alpha1, alpha2, True).play())
-        #print(result)
     return result
 
 result = simluation(2,2)
 from temp10 import count
+
+#run 10 of 10000 simulations.
 for i in range(10): print(count(simluation(2,2)))
